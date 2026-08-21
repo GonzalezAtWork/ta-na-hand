e57421f1860aa9b9517415007ad7a2e56f573412
handallleite13 <miniozzy.almeida@gmail.com>
2026-08-20 21:45:30 -0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const path = require('path');
+const Scraper = require('./scraper');
+
+const app = express();
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+
+let currentScraper = null;
+
+app.get('/api/search', (req, res) => {
+  const query = req.query.q;
+  const category = req.query.c || 'todas';
+  if (!query) return res.status(400).send('Query required');
+
+  // SSE Setup
+  res.writeHead(200, {
+    'Content-Type': 'text/event-stream',
+    'Cache-Control': 'no-cache',
+    'Connection': 'keep-alive'
+  });
+
+  const emit = (type, data) => {
+    res.write('event: ' + type + '\ndata: ' + JSON.stringify(data) + '\n\n');
+  };
+
+  if (currentScraper) {
+    currentScraper.stop();
+  }
+
+  currentScraper = new Scraper(emit);
+  
+  currentScraper.run(query, category).then(() => {
+    emit('finished', true);
+    res.end();
+  }).catch(err => {
+    emit('log', 'Erro interno: ' + err.message);
+    emit('finished', false);
+    res.end();
+  });
+
+  req.on('close', () => {
+    if (currentScraper) currentScraper.stop();
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log('Servidor rodando na porta ' + PORT);
+});
